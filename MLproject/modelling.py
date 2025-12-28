a8cbd0f46b489b69d4cdb891fcbafee7f94b3cc6
10222017-Kansha <kanshafirmansyah@gmail.com>
2025-12-28 15:56:32 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -11,10 +11,12 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "train_pca.csv"
+    file_path = (
+        sys.argv[3]
+        if len(sys.argv) > 3
+        else os.path.join(os.path.dirname(__file__), "train_pca.csv")
     )
+
     data = pd.read_csv(file_path)
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -23,8 +25,6 @@ if __name__ == "__main__":
         random_state=42,
         test_size=0.2
     )
-
-    input_example = X_train.iloc[:5]
 
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
@@ -35,13 +35,9 @@ if __name__ == "__main__":
     )
 
     model.fit(X_train, y_train)
-
     accuracy = model.score(X_test, y_test)
 
-    # sebelum log_param / log_metric
-    if mlflow.active_run() is None:
-        mlflow.start_run()
-    
+    # logging TANPA start_run
     mlflow.log_param("n_estimators", n_estimators)
     mlflow.log_param("max_depth", max_depth)
     mlflow.log_metric("accuracy", accuracy)
@@ -49,5 +45,5 @@ if __name__ == "__main__":
     mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
-        input_example=input_example
+        input_example=X_train[:5]
     )
