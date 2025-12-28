00d91cf908f45104ed98ff13799a822a4663f0f2
10222017-Kansha <kanshafirmansyah@gmail.com>
2025-12-28 16:05:08 +0700

    Refactor modelling.py for streamlined model training

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -3,47 +3,23 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import os
-import numpy as np
-import warnings
-import sys
 
-if __name__ == "__main__":
-    warnings.filterwarnings("ignore")
-    np.random.seed(40)
+data = pd.read_csv("train_pca.csv")
 
-    file_path = (
-        sys.argv[3]
-        if len(sys.argv) > 3
-        else os.path.join(os.path.dirname(__file__), "train_pca.csv")
-    )
+X_train, X_test, y_train, y_test = train_test_split(
+    data.drop("Credit_Score", axis=1),
+    data["Credit_Score"],
+    test_size=0.2,
+    random_state=42
+)
 
-    data = pd.read_csv(file_path)
+model = RandomForestClassifier(n_estimators=505, max_depth=37)
+model.fit(X_train, y_train)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        data.drop("Credit_Score", axis=1),
-        data["Credit_Score"],
-        random_state=42,
-        test_size=0.2
-    )
+accuracy = model.score(X_test, y_test)
 
-    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
-    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+mlflow.log_param("n_estimators", 505)
+mlflow.log_param("max_depth", 37)
+mlflow.log_metric("accuracy", accuracy)
 
-    model = RandomForestClassifier(
-        n_estimators=n_estimators,
-        max_depth=max_depth
-    )
-
-    model.fit(X_train, y_train)
-    accuracy = model.score(X_test, y_test)
-
-    # logging TANPA start_run
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_metric("accuracy", accuracy)
-
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=X_train[:5]
-    )
+mlflow.sklearn.log_model(model, "model")
